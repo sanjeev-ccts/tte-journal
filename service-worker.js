822b9dd4bf6900b7bb1c21ff8e7d901c0075054c
sanjeev-ccts <ksanjeev.nit@gmail.com>
2026-06-24 06:22:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,45 @@
-const CACHE_NAME = 'tte-journal-v1.6';
-const urlsToCache = [
+const CACHE_NAME = 'tte-journal-v1.2'; // Change this number when you update index.html
+const ASSETS_TO_CACHE = [
+  './',
   './index.html',
   './manifest.json',
-  './icon.png'
+  './icon.png' // Make sure you actually have an icon.png file in your GitHub!
 ];
 
-self.addEventListener('install', event => {
+// 1. Install Phase: Download the new files into the new box
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
       })
+      .then(() => self.skipWaiting()) // Forces the new version to activate immediately
   );
 });
 
-self.addEventListener('fetch', event => {
+// 2. Activate Phase: The Cleanup Crew (Deletes old version boxes)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName); // Throws away v1.1 when v1.2 arrives
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // Takes control of the screen immediately
+});
+
+// 3. Fetch Phase: How it serves the app to your phone
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
+        // Return the cached version if found, otherwise fetch from the internet
         return response || fetch(event.request);
       })
   );
