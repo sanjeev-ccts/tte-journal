fd6b2146c70ed1384d2d2753c1f242fef230fc3b
sanjeev-ccts <ksanjeev.nit@gmail.com>
2026-07-16 17:06:11 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'tte-tracker-v2.14';
+const CACHE_NAME = 'tte-tracker-v2.2';
 const ASSETS_TO_CACHE = [
     './index.html',
     './roster.html',
+    './i18n.js',
     './manifest.json'
 ];
 
